a56d9dca8c061d88917f1ccbb157ff1e298e4669
jrthompson25 <jrthompson35@outlook.com>
2022-02-24 21:15:56 -0600

    Running queries that will help answer project questions

--- scripts/app_store_project.sql
+++ scripts/app_store_project.sql
@@ -280,12 +280,123 @@
 	
 
 	
+--Queries from Class 	
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name;	
 	
-	
-	
-	
-	
-	
+--Apps that are less than a $1	
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+where a.price < 1;
+
+
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name;
+
+
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+where a.price < 1;
+
+
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price < 1;
+
+
+--Trying to combine the ratings
+SELECT a.name, 
+SUM(CAST(p.rating AS float), CAST(a.rating AS float)) AS combined_rating 
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price < 1
+
+
+SELECT SUM(p.rating + a.rating) AS combined_rating
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price < 1
+
+--TOP 10 Apps with highest review count (app_store)
+SELECT name, rating, CAST(review_count AS float) 
+FROM app_store_apps
+WHERE rating >= 4
+ORDER BY rating DESC, review_count DESC
+LIMIT 50;
+
+SELECT name, rating, review_count
+FROM app_store_apps
+WHERE rating >= 4
+ORDER BY rating DESC, review_count DESC
+LIMIT 50;
+		
+
+/*WITH app_store_rating AS
+SELECT name, CAST(review_count AS float), rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC
+LIMIT 50;
+
+WITH play_store_rating AS 
+SELECT name, review_count, rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY rating DESC, review_count DESC
+LIMIT 50;
+
+
+SELECT * 
+FROM play_store_rating
+INNER JOIN app_store_rating
+USING name;*/
+
+/*WITH app_store_rating AS
+SELECT name, CAST(review_count AS float), rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC
+LIMIT 50;
+
+WITH play_store_rating AS 
+SELECT name, review_count, rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY rating DESC, review_count DESC
+LIMIT 50;
+
+
+WITH app_store_rating AS
+SELECT name, CAST(review_count AS float), rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC
+LIMIT 50;
+
+WITH play_store_rating AS 
+SELECT name, review_count, rating
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC
+LIMIT 50;
+
+
+SELECT name, a.review_count, p.review_count rating, a.rating, p.rating
+FROM play_store_rating as p
+INNER JOIN app_store_rating as a
+ON a.name = p.name; */
 
 
 
