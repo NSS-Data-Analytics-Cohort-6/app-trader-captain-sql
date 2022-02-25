11e38ca4e626eaa1b5e5f0ec0164818bc354a096
Michael Weston <mew4vmtsu@gmail.com>
2022-02-24 21:13:52 -0600

    research 2_24

--- scripts/app-trader-captain-sql.sql
+++ scripts/app-trader-captain-sql.sql
@@ -1,4 +1,28 @@
 select *
 from app_store_apps
 where price < 1.00
-Order by price desc
+Order by price desc;
+
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name;
+
+SELECT *
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price < 1;
+
+SELECT SUM(p.rating + a.rating) AS combined_rating
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price < 1;
+
+SELECT name, CAST(review_count AS float), rating
+FROM app_store_apps
+WHERE rating >= 4
+ORDER BY rating DESC, review_count DESC
+LIMIT 100;
+
