51315d0d7abf27c5b2697472da8ff65d118cb3ef
Larry Scott II <larryscottii@Larrys-MacBook-Pro.local>
2022-02-26 12:55:50 -0600

    Updated answers to questions and working on excel visualizations

--- ss.sql
+++ ss.sql
@@ -10,12 +10,10 @@ INNER JOIN play_store_apps AS p
 ON a.name = p.name
 where a.price < 1;
 
-
 SELECT *
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name;
-
 
 SELECT *
 FROM app_store_apps AS a
@@ -23,13 +21,11 @@ INNER JOIN play_store_apps AS p
 ON a.name = p.name
 where a.price < 1;
 
-
 SELECT *
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE a.price < 1;
-
 
 --Trying to combine the ratings
 SELECT a.name,
@@ -38,12 +34,12 @@ FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE a.price < 1
+
 SELECT SUM(p.rating + a.rating) AS combined_rating
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE a.price < 1
-
 
 --TOP 10 Apps with highest review count (app_store)
 SELECT name, CAST(review_count AS float), rating
@@ -51,13 +47,14 @@ FROM app_store_apps
 WHERE rating >= 4
 ORDER BY review_count DESC, rating DESC
 LIMIT 50;
+
 SELECT name, review_count, rating
 FROM app_store_apps
 WHERE rating >= 4
 ORDER BY review_count DESC, rating DESC
 LIMIT 50;
 		
-/*WITH app_store_rating AS
+*WITH app_store_rating AS
 
 SELECT name, CAST(review_count AS float), rating
 FROM app_store_apps
@@ -66,7 +63,6 @@ ORDER BY review_count DESC
 LIMIT 50;
 
 WITH play_store_rating AS
-
 
 SELECT name, review_count, rating
 FROM app_store_apps
@@ -77,18 +73,16 @@ LIMIT 50;
 SELECT *
 FROM play_store_rating
 INNER JOIN app_store_rating
-USING 
+USING name;*
 
-/*WITH app_store_rating AS
-
-SELECT name, CAST(review_count AS float), rating
+*WITH app_store_rating AS
+SELECT name, CAST(review_count AS float),rating
 FROM app_store_apps
 WHERE rating > 4
 ORDER BY review_count DESC
 LIMIT 50;
 
 WITH play_store_rating AS
-
 SELECT name, review_count, rating
 FROM app_store_apps
 WHERE rating > 4
@@ -113,3 +107,44 @@ SELECT name, a.review_count, p.review_count rating, a.rating, p.rating
 FROM play_store_rating as p
 INNER JOIN app_store_rating as a
 ON a.name = p.name;
+
+Query for comparing rating and review count (only $1 or less apps):
+
+SELECT DISTINCT TRIM(a.name),
+CAST(a.review_count AS int) AS a_review_count,
+MAX(p.review_count) AS p_review_count,
+a.rating AS a_rating,
+p.rating AS p_rating,
+SUM(CAST(a.review_count AS int) + p.review_count) AS total_review_counts,
+p.genres,
+a.primary_genre
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price <= 1 AND a.rating >= 4.5 AND p.rating >= 4.5
+GROUP BY a.review_count, a.rating, p.rating, a.name,  a.primary_genre, p.genres
+ORDER BY total_review_counts DESC;
+
+SELECT DISTINCT a.name,
+CAST(a.review_count AS int) AS a_review_count,
+MAX(p.review_count) AS p_review_count,
+a.rating AS a_rating,
+p.rating AS p_rating,
+SUM(CAST(a.review_count AS int) + p.review_count) AS total_review_counts,
+p.genres,
+a.primary_genre
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.price <= 1 AND a.rating >= 4.5 AND p.rating >= 4.5
+GROUP BY a.review_count, a.rating, p.rating, a.name,  a.primary_genre, p.genres
+ORDER BY total_review_counts;
+
+
+SELECT COUNT(primary_genre) AS count_of_genre, 
+		primary_genre, 
+		COUNT(primary_genre)/7197*100 AS percent
+			FROM app_store_apps
+GROUP BY primary_genre	
+ORDER BY count_of_genre DESC;
+
